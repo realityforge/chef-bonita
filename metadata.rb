31e023daf79121e83517cdc231f557b8b123f934
Peter Donald <peter@realityforge.org>
2012-06-20 13:41:45 +1000

    Bump the version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "peter@realityforge.org"
 license          "Apache 2.0"
 description      "Installs/Configures the bonita BPM server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.0"
+version          "0.1.1"
 
 depends "tomcat"
 
