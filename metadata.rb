8c5b86b611379f17ea23341e1cd690c783f8bc64
Peter Donald <peter@realityforge.org>
2012-06-20 13:56:09 +1000

    Update to use markdown for documentation as it is the preferred form of documentation

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Peter Donald"
 maintainer_email "peter@realityforge.org"
 license          "Apache 2.0"
 description      "Installs/Configures the bonita BPM server"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.1"
 
 depends "tomcat"
