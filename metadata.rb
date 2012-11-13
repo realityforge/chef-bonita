67cae2f45a25590705fe1faa2271dcf665194f0d
Peter Donald <peter@realityforge.org>
2012-11-13 15:28:28 +1100

    Remove out of date documentation

--- metadata.rb
+++ metadata.rb
@@ -7,8 +7,3 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.3"
 
 depends "tomcat"
-
-attribute "bonita/package_url",
-  :display_name => "URL of bonita package",
-  :description => "The url of package to download to install bonita.",
-  :type => "string"
